0132d6c25f64b27ed71a3f69baad9a6c9b1d8bdf
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-10-24 18:28:57 -0400

    configuring mysql

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express   = require('express'),
       mysql     = require('mysql'),
-      hbs       = require('hbs');
+      hbs       = require('hbs'),
+      sql       = require('../utils/sql'),
+      router    = express.Router();
 
     const app = express()
     app.listen(3000)
@@ -9,15 +11,31 @@ const express   = require('express'),
     app.use(express.static('public'));
     app.set('view engine', 'hbs');
 
-    //Views
+    //Routes
         //Home
-        app.get('/', function (req, res) {
+        router.get('/', (req,res)=>{
+            console.log('Home route.')
             res.render('home')
-            console.log("Home page.")
+
+            //let query = `SELECT * FROM tbl_card WHERE profID="${req.params.id}"`;
+            sql.query(query, (err, result) => {
+                if (err) { throw err; console.log(err);
+                }})
         })
 
-        //Error Page
-        app.get('*', function (req, res) {
+        //Dynamic
+        router.get('/thing:id', (req,res)=>{
+            console.log('Dynamic route.')
+            console.log(req.params.id)
+
+            //let query = `SELECT * FROM tbl_card WHERE profID="${req.params.id}"`;
+            sql.query(query, (err, result) => {
+                if (err) { throw err; console.log(err);
+                }})
+        })
+
+        //Error
+        router.get('*', (req,res)=>{
+            console.log('error 404: page not found')
             res.render('error')
-            console.log("Error 404: Page not found.")
         })
---
+++ utils/config.js
@@ -0,0 +1,9 @@
+const config = {
+    host: "localhost",
+    port: 3000, // 8889 for mac, 3000 for windows
+    uname: "root",
+    pword: "", // leave blank for windows, "root" for mac
+    database: "" //link db here
+}
+
+module.exports = config;
