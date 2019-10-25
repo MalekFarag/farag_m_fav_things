33b8612c8c7938203530aa8441a81d8f868f310f
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-10-25 18:51:36 -0400

    Delete config.js

--- utils/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    host: "localhost",
-    port: 3000, // 8889 for mac, 3000 for windows
-    uname: "root",
-    pword: "", // leave blank for windows, "root" for mac
-    database: "db_fav_things" //link db here
-}
-
-module.exports = config;
