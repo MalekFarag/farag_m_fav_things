b0f5c265c14952efd04e6eee1c49593a84c8f673
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-10-25 18:53:18 -0400

    data from db being successfully pulled in console

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 const express   = require('express'),
-      mysql     = require('mysql'),
+      sql       = require('mysql'),
       hbs       = require('hbs'),
-      sql       = require('../utils/sql'),
       router    = express.Router();
 
-    const app = express()
-    app.listen(3000)
-    console.log('Connected to port 3000.')
 
-    app.use(express.static('public'));
-    app.set('view engine', 'hbs');
+
+    //Mysql config
+        const db = sql.createConnection({
+            host: 'localhost',
+            port: 3000,// 8889 for mac, 3000 for windows
+            user: 'root',
+            password: '',// leave blank for windows, "root" for mac
+            database: 'db_fav_things' // link db here
+        });
+
+        //connecting to db
+        db.connect(()=>{ 
+            console.log('Mysql connected.')
+        });
+
+        const pool = sql.createPool({
+            connectionLimit : 10,
+            host: "localhost",
+            user: "root",
+            password: "",
+            database: "db_fav_things"
+        });
+
+        const app = express()
+        app.listen(3000)
+        console.log('Connected to port 3000.')
+
+        app.use(express.static('public'));
+        app.set('view engine', 'hbs');
+
+
+
 
     //Routes
         //Home
@@ -38,4 +64,14 @@ const express   = require('express'),
             console.log('error 404: page not found')
             console.log(err)
             res.render('error')
+        })
+
+
+        //selecting artists as test
+            //able to put query into array for convenience
+        let query = [`SELECT name FROM tbl_things WHERE thing='music'`];
+        pool.query(query[0], (err, result) => {
+            if (err) { throw err; console.log(err);
+            }
+            console.log(result)
         })
